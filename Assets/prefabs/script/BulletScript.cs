d1158d61abf1202caabc6f887e9ee34e894657dd
edwardelric82 <edwardelric82@gmail.com>
2019-07-06 05:51:43 +0200

    Ancora pt4

---
+++ Assets/prefabs/script/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "enemy")
+        {
+            Destroy(collision.gameObject);
+
+            gameObject.SetActive(false);
+        }
+    }
+
+
+}
---
+++ Assets/prefabs/script/Shooting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    float bulletSpeed = 1100;
+    public GameObject bullet;
+
+    //AudioSource bulletAudio;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //bulletAudio = GetComponent<AudioSource>();
+
+    }
+
+    void Fire()
+    {
+        //Shoot
+        GameObject tempBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+        Rigidbody tempRigidBodyBullet = tempBullet.GetComponent<Rigidbody>();
+        tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+        Destroy(tempBullet, 0.5f);
+
+        //Play Audio
+        //bulletAudio.Play();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+
+    }
+}
